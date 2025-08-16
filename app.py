716e66e47ccea0a71fe909cc45847f20a7aa9649
cyrus-f <cyrus.forudi@gmail.com>
2025-08-16 12:56:52 +1000

    display json data on the site

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    # Load JSON file
+    with open("results.json", "r") as f:
+        events = json.load(f)
+    # Pass events to template
+    return render_template("events.html", events=events)
 
 if __name__ == "__main__":
     app.run(debug=True)
